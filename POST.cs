9114425defca3a33f4441cbbe45db27464e5589a
Flo <flo@uwap.org>
2023-09-27 12:23:45 +0200

    Added a POST request handler.

---
+++ POST.cs
@@ -0,0 +1,24 @@
+﻿namespace uwap.WebFramework.Plugins;
+
+public partial class NotesPlugin : Plugin
+{
+    public override async Task Handle(PostRequest req, string path, string pathPrefix)
+    {
+        if (req.User == null || (!req.LoggedIn))
+        {
+            req.Status = 403;
+            return;
+        }
+
+        var notes = GetOrCreate(req.User.Id);
+
+        string pluginHome = pathPrefix == "" ? "/" : pathPrefix;
+
+        switch (path)
+        {
+            default:
+                req.Status = 404;
+                break;
+        }
+    }
+}
