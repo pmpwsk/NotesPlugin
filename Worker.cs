517ede6c8e5d76f82c73b1630ed323498150f1a5
Flo <flo@uwap.org>
2023-09-29 23:37:04 +0200

    Changed Work() to split the table keys into user ID and user table name.

--- Worker.cs
+++ Worker.cs
@@ -13,7 +13,8 @@ public partial class NotesPlugin
                 if (item.Value.ParentId != null && !group.Value.Notes.ContainsKey(item.Value.ParentId))
                 {
                     dirty = true;
-                    group.Value.Delete(group.Key, item.Key, false);
+                    string[] key = group.Key.Split('_');
+                    group.Value.Delete(key[1], key[0], item.Key, false);
                 }
             }
             if (dirty) group.Value.UnlockSave();
