7c71f1cdd5537c2e9b30134262a70dedc34eb667
Flo <flo@uwap.org>
2024-02-22 00:14:52 +0100

    Added backwards compatibility for the old file system.

--- Init.cs
+++ Init.cs
@@ -2,5 +2,18 @@
 
 public partial class NotesPlugin : Plugin
 {
-    public NotesPlugin() => Directory.CreateDirectory("../Notes");
+    public NotesPlugin()
+    {
+        Directory.CreateDirectory("../Notes");
+
+        //backwards compatibility
+        foreach (var userTableDir in new DirectoryInfo("../Notes").GetDirectories("*", SearchOption.TopDirectoryOnly))
+        {
+            var files = userTableDir.GetFiles("*", SearchOption.TopDirectoryOnly);
+            foreach (var userId in files.Select(x => x.Name.Before('-')).Distinct())
+                Directory.CreateDirectory($"../Notes/{userTableDir.Name}/{userId}");
+            foreach (var file in files)
+                file.MoveTo($"../Notes/{userTableDir.Name}/{file.Name.Before('-')}/{file.Name.After('-')}");
+        }
+    }
 }
